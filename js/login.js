006dd642dbe35b5a3effbcac024b5d223eb2fd37
[Mariana dl] <[Marianadl97@gmail.com]>
2020-08-13 16:34:45 -0300

    Validacion login + register forms

--- js/login.js
+++ js/login.js
@@ -15,3 +15,45 @@ function is() {
     login.style.top = '5%';
     register.style.top = '50%';
 }
+
+// validación login form
+
+var userName = document.getElementById("userName");
+var loginPassword = document.getElementById("password");
+var loginBtn = document.getElementById("loginBtn");
+
+function checkLoginForm() {
+    if ( userName.value == "" || loginPassword.value == "") {
+        loginBtn.disabled = true;
+        alert("Debe completar todos los campos");
+        return false;
+    } else {
+        loginBtn.disabled = false;
+        return true;
+    }
+}
+
+userName.addEventListener("blur", checkLoginForm, false);
+loginPassword.addEventListener("blur", checkLoginForm, false);
+
+// validación register form
+
+var registerUserName = document.getElementById("registerUserName");
+var userEmail = document.getElementById("userEmail");
+var registerPassword = document.getElementById("registerPassword");
+var registerBtn = document.getElementById("registerBtn");
+
+function checkRegisterForm () { 
+    if ( registerUserName.value === "" || userEmail.value === "" || registerPassword.value === "") {
+        registerBtn.disabled = true;
+        alert("Debe completar todos los campos");
+        return false;
+    } else {
+        registerBtn.disabled = false;
+        return true;
+    }
+}
+
+registerUserName.addEventListener("blur", checkRegisterForm, false);
+userEmail.addEventListener("blur", checkRegisterForm, false);
+registerPassword.addEventListener("blur", checkRegisterForm, false);
