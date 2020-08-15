3007f1a3ca471bd76c50f5b280cde9838c41924d
[Mariana dl] <[Marianadl97@gmail.com]>
2020-08-15 00:19:34 -0300

    Corrección loop validaciones

--- js/login.js
+++ js/login.js
@@ -7,11 +7,12 @@ document.addEventListener("DOMContentLoaded", function(e){
 var loginOpt = document.getElementById('inicio_sesion');
 var registerOpt = document.getElementById('crear_cuenta');
 
-
+// función onclick btn Crear Cuenta
 function cc() {
     loginOpt.style.top = '80%';
     registerOpt.style.top = '-50%';
 }
+// función onclick btn Iniciar Sesion
 function is() {
     loginOpt.style.top = '5%';
     registerOpt.style.top = '50%';
@@ -34,39 +35,42 @@ var loginPassword = document.getElementById("password");
 var loginBtn = document.getElementById("loginBtn");
 
 
-
 function checkLoginForm() {
     if ( userName.value == "" || loginPassword.value == "") {
-        loginBtn.disabled = true;
         alert("Debe completar todos los campos");
         return false;
     } else {
-        loginBtn.disabled = false;
         return true;
     }
 }
 
-userName.addEventListener("blur", checkLoginForm, false);
-loginPassword.addEventListener("blur", checkLoginForm, false);
-/*
 // validación register form
 
 var registerUserName = document.getElementById("registerUserName");
 var userEmail = document.getElementById("userEmail");
 var registerPassword = document.getElementById("registerPassword");
+var registerPassword2 = document.getElementById("registerPassword2");
 var registerBtn = document.getElementById("registerBtn");
+var registerCheckBox = document.getElementById("registerCheckBox");
 
 function checkRegisterForm () { 
-    if ( registerUserName.value === "" || userEmail.value === "" || registerPassword.value === "") {
-        registerBtn.disabled = true;
+    if ( registerUserName.value === "" || userEmail.value === "" || registerPassword.value === "" || registerPassword2.value === "") {
         alert("Debe completar todos los campos");
         return false;
+    } 
+    if ( registerPassword.value !== registerPassword2.value ) {
+        alert("Las contraseñas deben coincidir");
+        return false;
+    } 
+    if (userEmail.validity.typeMismatch) {
+        alert("Debe ingresar un correo electrónico válido");
+        return false;
+    }
+    if ( !registerCheckBox.checked) {
+        alert("Debe Aceptar los términos y condiciones")
+        return false;
     } else {
-        registerBtn.disabled = false;
         return true;
     }
 }
 
-registerUserName.addEventListener("blur", checkRegisterForm, false);
-userEmail.addEventListener("blur", checkRegisterForm, false);
-registerPassword.addEventListener("blur", checkRegisterForm, false); */
