f53b397ea5d4f841961e2bf0b8162065146e8ec0
[Mariana dl] <[Marianadl97@gmail.com]>
2020-08-16 16:53:22 -0300

    ;

--- js/init.js
+++ js/init.js
@@ -44,7 +44,7 @@ function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');
-    location.href ="index.html"
+    location.href ="index.html";
   });
 }
 
