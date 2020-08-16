773a2d7cc9b113ec79814a8d217e50953cc82212
[Mariana dl] <[Marianadl97@gmail.com]>
2020-08-16 16:59:35 -0300

    sO

--- js/init.js
+++ js/init.js
@@ -43,6 +43,7 @@ var getJSONData = function(url){
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
+    localStorage.clear()
     console.log('User signed out.');
     location.href ="index.html";
   });
