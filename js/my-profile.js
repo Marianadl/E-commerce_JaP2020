492d71b86e9cab41ad6ca1054c6a42df0853107f
[Mariana dl] <[Marianadl97@gmail.com]>
2020-11-08 23:07:50 -0300

    SweetAlert

--- js/my-profile.js
+++ js/my-profile.js
@@ -24,15 +24,33 @@ document.addEventListener("DOMContentLoaded", function (e) {
             }
             document.getElementById("selectCountry").innerHTML += opcionPais;
         }
-
-
-        
     });
     
     // Si los datos estan en localStorage en newUserData ejecuta showUserData, si no estan ejecuta showNoData
    if (localStorage.getItem("newUserData") != undefined) {
         showUserData()
    } else { showNoData() }
+
+   
+document.getElementById("changeInfo").addEventListener("click", function () {
+    Swal.fire({
+        title: '¿Confirmar cambios?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#d33a57',
+        cancelButtonColor: '#ec9ca7',
+        confirmButtonText: 'Sí, cambiar datos'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            '',
+            'Los cambios se guardaron con éxito',
+            'success'
+          )
+          editData();
+        }
+      })
+})
 });
 
 // var contiene datos de usuario
@@ -55,30 +73,28 @@ function editData() {
     userData.country = document.getElementById("selectCountry").value;
     userData.email = document.getElementById("email").value;
 
-    // Función para tomar el file del input showImg y convertirlo a string base64. Cambia el src de img al string base64
-    function previewFile() {
-        const preview = document.getElementById("showImg");
-        const file = document.querySelector('input[type=file]').files[0];
-        const reader = new FileReader();
-      
-        reader.addEventListener("load", function () {
-          // convert image file to base64 string
-          preview.src = reader.result;
-        }, false);
-      
-        if (file) {
-          reader.readAsDataURL(file);
-        }
-
-        
-      }
-     
-previewFile();
-userData.img = document.getElementById("showImg").src; //Toma el src base64 del file que se subió y lo guarda como atributo de la var userData
+previewFile()
+userData.img = document.getElementById("showImg").src;
 localStorage.setItem("newUserData", JSON.stringify(userData)); // guardo var userData como string en logalStorage como newUserData
 showUserData(); //Ejecuto función para mostrar los datos modificados
         }
 
+ // Función para tomar el file del input showImg y convertirlo a string base64. Cambia el src de img al string base64
+ function previewFile() {
+    const preview = document.getElementById("showImg");
+    const file = document.querySelector('input[type=file]').files[0];
+    const reader = new FileReader();
+  
+    reader.addEventListener("load", function () {
+      // convert image file to base64 string
+      preview.src = reader.result;
+      
+    }, false);
+  
+    if (file) {
+     reader.readAsDataURL(file);
+    }
+  }
 
 //Función para mostrar los datos modificados y guardados en localStorage
 function showUserData() {
@@ -94,8 +110,7 @@ function showUserData() {
                 if (userData.country != "") {document.getElementById("showCountry").innerHTML = userData.country;} else {
                     document.getElementById("showCountry").innerHTML = "País";}
                 if (userData.email != "") {document.getElementById("showEmail").href = ` mailto:${userData.email} `;}
-                if (userData.img != "") {document.getElementById("showImg").src = ` ${userData.img}`} else {
-                    document.getElementById("showImg").src = "https://picsum.photos/200/200"; }
+                if (userData.img != "") {document.getElementById("showImg").src = ` ${userData.img}`}
             }
 
 // Funcion a ejecutar antes de guardar en localStorage
